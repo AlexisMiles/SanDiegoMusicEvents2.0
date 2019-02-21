6c6525233cc3163f2486cbc465079cd6e2e004b9
4803AW07 <4803AW07@usa.miracosta.cc.ca.us>
2019-02-21 14:56:50 -0800

    Initial commit

---
+++ app/src/main/java/edu/miracostacollege/cs134/sandiegomusicevents/MusicEventListAdapter.java
@@ -0,0 +1,75 @@
+package edu.miracostacollege.cs134.sandiegomusicevents;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import edu.miracostacollege.cs134.sandiegomusicevents.model.MusicEvent;
+
+public class MusicEventListAdapter extends ArrayAdapter<MusicEvent> {
+
+    //Declare member variables to store the parameters (context, resource id, List of <MusicEvent>)
+    private Context mContext;
+    private int mResourceId;
+    private List<MusicEvent> mAllEvents;
+
+    //This constructor is being called by MainActivity
+
+    public MusicEventListAdapter(Context context, int resource, @NonNull List<MusicEvent> objects){
+        super(context, resource, objects);
+        mContext = context;
+        mResourceId = resource;
+        mAllEvents = objects;
+    }
+
+    //In order to bridge the View (music_event_list_item) with the Model (MusicEvent), we override:
+    //Ctrl + o to Override
+
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        // Inflate out custom layout from List<MusicEvent>
+        MusicEvent focusedEvent = mAllEvents.get(position);
+
+        //Manually inflate the custom layout
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        //Tell the inflater to inflate music_event_list_item
+        View customView = inflater.inflate(mResourceId, null);
+
+        //Fill the parts of the custom view
+        ImageView listItemImageView = customView.findViewById(R.id.listItemImageView);
+        TextView listItemArtistTextView = customView.findViewById(R.id.listItemArtistTextView);
+        TextView listItemDateTextView = customView.findViewById(R.id.listItemDateTextView);
+
+        listItemArtistTextView.setText(focusedEvent.getArtist());
+        listItemDateTextView.setText(focusedEvent.getDate());
+
+        //Load the image dynamically
+        AssetManager am = mContext.getAssets();
+        try {
+            InputStream stream = am.open(focusedEvent.getImageName());
+            Drawable image = Drawable.createFromStream(stream, focusedEvent.getArtist());
+            listItemImageView.setImageDrawable(image);
+        } catch (IOException e) {
+            Log.e("SD Music Events", e.getMessage());
+        }
+
+        //Return customView, inflated with all information
+        return customView;
+    }
+}
